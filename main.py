ed71723580115db5c2773bd2db520efc8727d3d0
Marta <marta.krzyk.dev@gmail.com>
2020-04-29 20:25:15 +0200

    Refactor main

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from Project4_Split.SpitGame import SpitGame
 
 game = SpitGame()
@@ -8,19 +7,3 @@ game.CreatePlayers()
 while True:
     game.PlayRound()
     game.PrintScores()
-=======
-from Project4_Split.SpitGame import SpitGame
-
-
-game = SpitGame()
-game.PrintTitle()
-game.CreatePlayers()
-
-result = game.Play()
-#game.PrintResult()
-# play again with same players
-# save score
-#while True:
-#   input = input()
-#  Command(input())
->>>>>>> a8b2937820a192227135ebe91399e7f31b4c7084
