7d0a71c40759b77cdf888b486defb0b70586aab8
Marta <marta.krzyk.dev@gmail.com>
2020-05-11 13:18:48 +0200

    Click enter to start a new game

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from Project4_Split.SpitGame import SpitGame
 
 game = SpitGame()
 game.PrintTitle()
-
-game.CreatePlayers(ask_for_names=False)
+game.CreatePlayers(ask_for_names=True)
 
 while True:
     game.PlayGame()
+    game.GetInput("Click enter to have another game...")
     game.PrintTitle()
