ed468fb11e8544d00f6c2850bfe84889a7b360b9
Marta <marta.krzyk.dev@gmail.com>
2020-05-12 20:52:44 +0200

    Don't clear screen after every move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from Project4_Split.SpitGame import SpitGame
 
-game = SpitGame(clear_console_with_every_turn_=True)
+game = SpitGame(clear_console_with_every_turn_=False)
 game.PrintTitle()
 game.CreatePlayers(ask_for_names=True)
 
